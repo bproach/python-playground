603d53edf77a93adc62c6437b8eef81f1f3313c5
Brendan Roach <brendan.roach.6@gmail.com>
2017-01-11 16:36:12 -0500

    add JavaScript calculator

---
+++ calculator.js
@@ -0,0 +1,17 @@
+/// Addition
+function add(a,b){
+	return a+b
+};
+
+///Subtraction
+function subt(a,b){
+	return a-b
+};
+///Multiply
+function mult(a,b){
+	return a*b
+};
+///Divide
+function div(a,b){
+	return a/b
+};
